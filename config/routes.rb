405ca901e504cf5858ed6fcd29c724113497fc94
root <root@debian5.localdomain>
2010-05-10 00:19:19 -0500

    Removed default Rails page - fixed error

--- config/routes.rb
+++ config/routes.rb
@@ -34,10 +34,10 @@ ActionController::Routing::Routes.draw do |map|
   #     admin.resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'pages', :action => 'home'  #jjk
-  root_path => '/'                #jjk
-  root_url => 'localhost:3000/'   #jjk
+  # You can have the root of your site routed with map.root 
+  # -- just remember to delete public/index.html.
+  map.root :controller => 'pages', :action => 'home'
+  
 
   # See how all your routes lay out with "rake routes"
 
