a151d8eeb39563dac7dd8552c01027f4dce9650c
root <root@debian5.localdomain>
2010-05-11 22:57:24 -0500

    fixed some errors with gravatar plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-    # Return a title on a per-page basis.               # Documentation comment
-    def title                                           # Method definition
-    base_title = "Ruby on Rails Tutorial Sample App"    # Variable assignment
-    if @title.nil?                                      # Boolean test for nil
-     base_title                                         # Implicit return
+
+  # Return a title on a per-page basis.               # Documentation comment
+  def title                                           # Method definition
+  base_title = "Ruby on Rails Tutorial Sample App"    # Variable assignment
+    if @title.nil?                                    # Boolean test for nil
+      base_title                                      # Implicit return
     else
-      "#{base_title} | #{@title}"                       # String interpolation
+      "#{base_title} | #{h(@title)}"                  # String interpolation
     end
   end
 end
+
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,3 +28,8 @@ config.action_mailer.delivery_method = :test
 # config.active_record.schema_format = :sql
 
   config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+
+# Settings specified here will take precedence over those in
+# config/environment.rb
+config.gem 'factory_girl'
+
